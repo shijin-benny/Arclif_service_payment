b5dd2c8de96d1a5ae8b166c146b7cd9a201f98ce
shijin-benny <mr.shijinbenny361@gmail.com>
2022-05-06 16:12:27 +0530

    check error

--- controller/paymentHelper.js
+++ controller/paymentHelper.js
@@ -4,53 +4,55 @@ const sendMail = require("../controller/Nodemailer/nodemailer");
 
 module.exports = {
     //<!===== create payment order and inset userid,orderId to database =======/> //
-    paymentOrder:(req,res)=>{
-       razorpayPayment.createOrder(req.body.amount).then(order=>{
-           if(order.status === 'created'){
-               const paymentData = paymentSchema({
-                     orderId:order.id,
-                     userId:req.body.userId,
-               })
-                paymentData.save().then(data=>{
-                    res.json({status:200,order:order});
+    paymentOrder: (req, res) => {
+        razorpayPayment.createOrder(req.body.amount).then(order => {
+            if (order.status === 'created') {
+                const paymentData = paymentSchema({
+                    orderId: order.id,
+                    userId: req.body.userId,
+                })
+                paymentData.save().then(data => {
+                    res.json({ status: 200, order: order });
                 }
-                ).catch(err=>{
+                ).catch(err => {
                     res.json(err);
                 })
-           }
-    }).catch(err=>{
-        res.json(err);
-    })
+            }
+        }).catch(err => {
+            res.json(err);
+        })
     },
     //<!========= verify payment and update payment status to database ========/>//
-    paymentVerify:(req,res)=>{
+    paymentVerify: (req, res) => {
         console.log(req.body);
         console.log(req.params.id);
-        razorpayPayment.verifyPayment(req.body).then(order=>{
-           paymentSchema.updateOne({userId:req.params.id},{
-            
-                paymentId:order.id,
-                amount:order.amount / 100,
-                paymentStatus:order.status,
-                method:order.method,
-                email:order.email,
-                orderId:order.order_id,
-                fee : order.fee / 100,
-                tax : order.tax / 100,
-              },(err,data)=>{
-                if(err){
-                    res.json({error:err,message:'Payment updation failed'});
-                }else{
-                    sendMail.receiptMail(order).then(mail=>{
-                        res.json({status:200,message:'Payment verified successfully'});
-                    }).catch(err=>{
-                        res.json({error:err,message:'Payment verified successfully but mail sending failed'});
+        razorpayPayment.verifyPayment(req.body).then(order => {
+            console.log(order);
+            paymentSchema.updateOne({ userId: req.params.id }, {
+                paymentId: order.id,
+                amount: order.amount / 100,
+                paymentStatus: order.status,
+                method: order.method,
+                email: order.email,
+                orderId: order.order_id,
+                fee: order.fee / 100,
+                tax: order.tax / 100,
+            }, (err, data) => {
+                console.log(data);
+                if (err) {
+                    res.json({ error: err, message: 'Payment updation failed' });
+                } else {
+                    sendMail.receiptMail(order).then(mail => {
+                        res.json({ status: 200, message: 'Payment verified successfully' });
+                    }).catch(err => {
+                        res.json({ error: err, message: 'Payment verified successfully but mail sending failed' });
                     })
                 }
             }
             )
-        }).catch(err=>{
-            res.json({status:500,message:'Payment not verified'});
+        }).catch(err => {
+            console.log(err);
+            res.json({ status: 500, message: 'Payment not verified' });
         })
     },
 }
